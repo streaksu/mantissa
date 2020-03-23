1c6d26aabc844dfcdb36cc636ec03accbf83bf1b
streaksu <assemblyislaw@gmail.com>
2020-03-23 02:16:39 +0100

    Add url guessing for local files

--- source/backend/url.d
+++ source/backend/url.d
@@ -1,11 +1,14 @@
 module backend.url;
 
 import std.regex;
+import std.file;
 
 private immutable NO_PROTOCOL = ctRegex!(r"^[a-zA-Z0-9\.]+[\.][a-zA-Z0-9]+$");
 
 string urlFromUserInput(string userURL) {
-    if (matchAll(userURL, NO_PROTOCOL)) {
+    if (isFile(userURL) || isDir(userURL)) {
+        return "file://" ~ userURL;
+    } else if (matchAll(userURL, NO_PROTOCOL)) {
         return "https://" ~ userURL;
     } else {
         return userURL;
